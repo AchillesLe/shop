b13ecdd9ca3d8fd6bcc087ca0d55ea43c642084a
AchillesLe <lvbao.cntt.sgu@gmail.com>
2018-10-06 13:06:39 +0700

    commit test category

--- shop-api/shop-api/Controllers/CategoryController.cs
+++ shop-api/shop-api/Controllers/CategoryController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using shop_api.Models;
 
 namespace shop_api.Controllers
 {
+    [RoutePrefix("api/category")]
     public class CategoryController : ApiController
     {
+        private ShopApiModel context = new ShopApiModel();
+        [HttpGet(),Route("getall")]
+        public List<Category> getALL()
+        {
+            return context.Categories.ToList();
+        }
     }
 }

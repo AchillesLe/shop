eda314f94ae2004d221c1535428888e6735e268e
Vu Phan <arsenalvu@gmail.com>
2018-10-28 11:54:30 +0700

    fix

---
+++ shop-api/shop-api/Controllers/BillController.cs
@@ -0,0 +1,24 @@
+﻿using shop_api.Service;
+using shop_api.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace shop_api.Controllers
+{
+    [RoutePrefix("api/bill")]
+    public class BillController : ApiController
+    {
+        BillService receiptService = new BillService();
+        [HttpGet(), Route("getall")]
+        public IHttpActionResult GetAll()
+        {
+            string token = Token.HandleToken(Request);
+            if (token == "") return BadRequest(Message.messageNotValidToken);
+            return Ok(receiptService.GetAll());
+        }
+    }
+}

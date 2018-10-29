758a238d40dd3a0a94c841aa9653cc2ff0de7325
AchillesLe <lvbao.cntt.sgu@gmail.com>
2018-10-29 21:18:57 +0700

    sửa model bi lỗi sau khi demo

--- shop-app/src/admin/services/http.js
+++ shop-app/src/admin/services/http.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class HTTP {
     constructor() {
         this.instance = axios.create({
-            baseURL: 'http://localhost/BanHangAPI/api/',
+            baseURL: 'http://localhost:5000/api/',
             //timeout: 1000,
         });
     }
